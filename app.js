da57877039cad45673af03343696dd0927769962
luqni <mluqnibaehaqi@gmail.com>
2024-09-22 19:16:14 +0700

    change location folder for session

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const clients = {};
 
 // Fungsi untuk mengecek apakah sesi ada atau tidak
 function sessionExists(email) {
-    const sessionPath = path.join(process.cwd(), 'sessions', email);
+    const sessionPath = path.join('/tmp', 'sessions', email);
     return fs.existsSync(sessionPath);
 }
 
@@ -58,7 +58,7 @@ function createClientForUser(email, res) {
     const client = new Client({
         authStrategy: new LocalAuth({
             clientId: sanitizedEmail,
-            dataPath: path.join(process.cwd(), 'sessions')
+            dataPath: path.join('/tmp', 'sessions')
         })
     });
 
